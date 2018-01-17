961c19d1472dd864f06d0c6017aa8590d139f2be
usarneme <jamestcoop@gmail.com>
2018-01-17 13:54:12 -0800

    Updated the imbusy script to show output more frequently.

--- fun/imbusy.sh
+++ fun/imbusy.sh
@@ -1,3 +1,3 @@
-while [ 1 ]; do dmesg | shuf -n 1; sleep $((1 + RANDOM % 10)); done
+while [ 1 ]; do dmesg | shuf -n 1; sleep $((1 + RANDOM % 2)); done
 
 # sudo chmod +x file.sh to make it executable with ./file.sh command
